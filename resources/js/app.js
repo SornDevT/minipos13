5aba3dd15b3de0d8c5c4ff188132fad8ab24bf77
SornDev <sorndev.teach@gmail.com>
2023-12-01 20:42:27 +0700

    install Template HTML5 and install vue-router VDO 03

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,10 @@ import './bootstrap';
 
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './router';
+import Menu from './components/MenuSidebar.vue';
 
 const app = createApp(App)
+app.use(router)
+app.component('MenuSideBar',Menu)
 app.mount('#app-vue');
---
+++ resources/js/pages/Store.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <h1>Store Page 11111</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Minipos13Store',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
