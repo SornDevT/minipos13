11bd96db6b577b8d7284390fcfda117f32e10ba2
SornDev <sorndev.teach@gmail.com>
2023-12-04 21:13:09 +0700

    laravel command created Controlle, Model and install JWT, Pinia VDO 04

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -8,6 +8,28 @@ import Transection from '../pages/Transection.vue';
 import Report from '../pages/Report.vue';
 import NoPage from '../pages/NoPage.vue';
 
+// middleware ກວດຊອບຂໍ້ມູນ ການເຂົ້າລະບົບ
+
+import { useStore } from "../store/auth";
+
+const authMiddleware = (to, from, next) => {
+    const token = localStorage.getItem('web_token');
+    const user = localStorage.getItem('web_user');
+    const store = useStore();
+
+    if(!token){
+        next({
+            path: '/login',
+            replace: true
+        })
+    } else {
+        // ຂຽນຂໍ້ມູນ token ແລະ user ເຂົ້າ Pinia
+        store.set_token(token);
+        store.set_user(user);
+        next();
+    }
+}
+
 export const routes = [
     {
         path:'/',
@@ -26,27 +48,42 @@ export const routes = [
     {
         name: 'store',
         path: '/store',
-        component: Store
+        component: Store,
+        meta: {
+            middleware: [authMiddleware]
+        }
     },
     {
         name: 'pos',
         path: '/pos',
-        component: Pos
+        component: Pos,
+        meta: {
+            middleware: [authMiddleware]
+        }
     },
     {
         name: 'transection',
         path: '/transection',
-        component: Transection
+        component: Transection,
+        meta: {
+            middleware: [authMiddleware]
+        }
     },
     {
         name: 'report',
         path: '/report',
-        component: Report
+        component: Report,
+        meta: {
+            middleware: [authMiddleware]
+        }
     },
     {
         name:'no_page',
         path: '/:pathMacth(.*)*',
-        component:NoPage
+        component:NoPage,
+        meta: {
+            middleware: [authMiddleware]
+        }
     }
 
 ];
@@ -58,5 +95,28 @@ const router = createRouter({
         window.scrollTo(0,0)
     }
 });
+
+router.beforeEach((to, from, next)=>{
+
+    const token = localStorage.getItem('web_token');
+    if(to.meta.middleware){
+        to.meta.middleware.forEach(middleware => middleware(to, from, next))
+    } else {
+        if(to.path == '/login' || to.path == '/'){
+            if(token){
+                next({
+                    path:'/store',
+                    replace:true
+                })
+            } else {
+                next();
+            }
+        } else {
+            next();
+        }
+    }
+
+});
+
 
 export default router;
