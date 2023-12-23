23f1d3d4101f5981308a5bf2d6a1383852dbd71e
SornDev <sorndev.teach@gmail.com>
2023-12-23 21:06:37 +0700

    Crested Transection Page and Created Report VDO 11

--- app/Http/Controllers/TransectionController.php
+++ app/Http/Controllers/TransectionController.php
@@ -16,6 +16,35 @@ class TransectionController extends Controller
         $this->middleware('auth:api');
     }
 
+    public function index(Request $request){
+
+        $sort = \Request::get('sort');
+        $perpage = \Request::get('perpage');
+        $month_type = $request->month_type;
+        $dmy = $request->dmy;
+
+        $m = explode("-",$dmy)[1];
+        $y = explode("-",$dmy)[0];
+
+        if($month_type == 'm'){
+
+            $tran = Transection::orderBy("id",$sort)
+            ->whereYear("created_at", $y)
+            ->whereMonth("created_at",$m)
+            ->paginate($perpage)
+            ->toArray();
+
+        } else if($month_type == 'y'){
+            $tran = Transection::orderBy("id",$sort)
+            ->whereYear("created_at", $y)
+            ->paginate($perpage)
+            ->toArray();
+        }
+
+        return array_reverse($tran);
+
+    }
+
     public function add(Request $request){
         try {
 
@@ -89,7 +118,7 @@ class TransectionController extends Controller
                         "bill_id" => $bill_id,
                         "name" => $item["name"],
                         "amount" => $item["order_amount"],
-                        "price" => $item["order_amount"]*$item["price_sell"]
+                        "price" => $item["price_sell"]
                     ]);
                     $bill_list->save();
 
@@ -103,7 +132,6 @@ class TransectionController extends Controller
                     ]);
 
             }
-
 
 
             $success = true;
